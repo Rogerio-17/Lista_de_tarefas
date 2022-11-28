244438421edf084465bd99072469cf9d1336fa2a
Rogerio-17 <rogeriojmf10@gmail.com>
2022-11-28 20:56:38 -0300

    Estilizando e funcionando botao sair

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import "./admin.css";
 
+import { auth } from "../../firebaseConnection";
+import { signOut } from "firebase/auth";
+
 function Admin() {
   const [tarefaInput, setTarefaInput] = useState("");
 
@@ -9,19 +12,38 @@ function Admin() {
 
     alert("Clicou");
   }
+
+  async function handleLogout() {
+    await signOut(auth);
+  }
   return (
     <div className="admin-container">
       <h1>Minhas tarefas</h1>
 
-      <form onSubmit={handleRegister}>
+      <form className="form" onSubmit={handleRegister}>
         <textarea
           placeholder="Digite sua tarefa"
           value={tarefaInput}
           onChange={(e) => setTarefaInput(e.target.value)}
         />
+
+        <button className="btn-register" type="submit">
+          Registrar tarefa
+        </button>
       </form>
 
-      <button type="submit">Registrar tarefa</button>
+      <article className="list">
+        <p>Estudar reactjs e javascript</p>
+
+        <div>
+          <button className="btn-edita">Editar</button>
+          <button className="btn-delete">Concluir</button>
+        </div>
+      </article>
+
+      <button className="btn-logout" onClick={handleLogout}>
+        Sair
+      </button>
     </div>
   );
 }
